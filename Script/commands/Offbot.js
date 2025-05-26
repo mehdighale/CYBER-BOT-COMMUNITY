7e3020e2775ffa54daa1405b852e9141bb5ac043
mehdighale <botyuna8@gmail.com>
2025-05-26 14:29:46 +0100

    Update Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -1,14 +1,15 @@
 module.exports.config = {
-	name: "offbot",
-	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-	description: "turn the bot off",
-	commandCategory: "system",
-	cooldowns: 0
-        };
-module.exports.run = ({event, api}) =>{
-    const permission = ["100015168369582", "61576357565998"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ULLASH", event.threadID, event.messageID);
-  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+    name: "غادر",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    description: "",
+    commandCategory: "Admin",
+    usages: "out [id]",
+    cooldowns: 10,
+};
+
+module.exports.run = async function({ api, event, args }) {
+        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
 }
