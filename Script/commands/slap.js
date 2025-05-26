d5ca09a24a6b7fb7c31d1a5787effe957f96e19b
mehdighale <botyuna8@gmail.com>
2025-05-26 14:45:01 +0100

    Update slap.js

--- Script/commands/slap.js
+++ Script/commands/slap.js
@@ -1,44 +1,44 @@
 module.exports.config = {
-  name: "slap",
+  name: "كف",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-  description: "Slap the friend tag",
-  commandCategory: "general",
-  usages: "slap [Tag someone you want to slap]",
+  credits: "يونو",
+  description: "اعطي واحد صفعة محترمة عالسريع",
+  commandCategory: "الضحك والتفريغ النفسي",
+  usages: "صفعة [منشن واحد من المتعوسين]",
   cooldowns: 5,
 };
 
-
 module.exports.run = async ({ api, event, args }) => {
-	const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-    var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
-  if (!args.join("")) return out("Please tag someone");
-  else
+  const axios = require('axios');
+  const request = require('request');
+  const fs = require("fs");
+  var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
+
+  if (!args.join("")) return out("⚠️ لازم تِمنشن واحد عشان نكفخه");
+
   return axios.get('https://api.waifu.pics/sfw/slap').then(res => {
-        let getURL = res.data.url;
-        let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
-        var mention = Object.keys(event.mentions)[0];
-                  let tag = event.mentions[mention].replace("@", "");    
-        
- let callback = function () {
-            api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-        api.sendMessage({
-						        body: "Slapped! " + tag + "\n\n*sorry, i thought there's mosquito*",
-                                          mentions: [{
+    let getURL = res.data.url;
+    let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
+    var mention = Object.keys(event.mentions)[0];
+    let tag = event.mentions[mention].replace("@", "");
+
+    let callback = function () {
+      api.setMessageReaction("✋", event.messageID, (err) => {}, true);
+      api.sendMessage({
+        body: `بووووم!! صفعة خرافية لـ ${tag} 💥\n\n🤣 آسف حسبتك بعوضة تتحرش`,
+        mentions: [{
           tag: tag,
-          id: Object.keys(event.mentions)[0]
+          id: mention
         }],
-						attachment: fs.createReadStream(__dirname + `/cache/slap.${ext}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/slap.${ext}`), event.messageID)
-				};
- //   }
-        request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/slap.${ext}`)).on("close", callback);
-			})
-    .catch(err => {
-                     api.sendMessage("Failed to generate gif, be sure that you've tag someone!", event.threadID, event.messageID);
-    api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
-                  })     
+        attachment: fs.createReadStream(__dirname + `/cache/slap.${ext}`)
+      }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/slap.${ext}`), event.messageID);
+    };
+
+    request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/slap.${ext}`)).on("close", callback);
+  })
+  .catch(err => {
+    api.sendMessage("❌ فشل تحميل الصفعة، يمكن اليد تعبت؟ جرب تاني!", event.threadID, event.messageID);
+    api.setMessageReaction("😿", event.messageID, (err) => {}, true);
+  });
 }
